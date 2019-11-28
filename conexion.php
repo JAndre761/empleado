9ba37ba07ed8982252c06a492dd7aafec120e502
JAndre <32041983+JAndre761@users.noreply.github.com>
2019-11-27 20:33:38 -0500

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 /*Datos de conexion a la base de datos*/
-$db_host = "localhost";
+$db_host = "mysql01.c4utrkzk5zgf.us-west-1.rds.amazonaws.com";
 $db_user = "root";
 $db_pass = "";
-$db_name = "test_empleados";
+$db_name = "empleados";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
